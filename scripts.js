475cfff0569f572798217bef3e74adb43cff693f
riveroNicolas <nicolasriv92@gmail.com>
2022-05-25 03:11:13 -0300

    incorporacion de js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var textoCopiado = document.querySelector(".form-principal");
+var textoDevuelto = document.querySelector(".form-secundario");
+
+function BotonEncriptar(){
+    var textoEncriptado = encriptar(textoCopiado.value);
+    textoDevuelto.value = textoEncriptado;
+}
+
+function encriptar(stringParaEncriptar){
+    var matrizCodigo = [["a","batman"],["e","riddler"],["i","catwoman"],["o","robin"],["u","joker"]]
+    stringParaEncriptar = stringParaEncriptar.toLowerCase();
+    for (let i=0; i< matrizCodigo.length; i++){
+        if(stringParaEncriptar.includes(matrizCodigo[i][0])){
+            stringParaEncriptar = stringParaEncriptar.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringParaEncriptar
+}
